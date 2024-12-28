3f7942c00b3045ba9892a3af5028c3795bd6c0b5
kszygenda <rszygenda2002@gmail.com>
2024-12-28 12:36:24 +0100

    repositories view. local github token for development

--- src/pages/RepositoriesPage.vue
+++ src/pages/RepositoriesPage.vue
@@ -1,75 +1,115 @@
 <template>
   <!-- the idea is to create 2 columns with reach repo having individual card-->
   <v-container fluid>
-    <h1>Repositories View</h1>
     <v-row>
-      <v-col
-        class="red-outline">
-        <v-row
-          class="red-outline"
-          v-for="n in ((repositories?.length || 0)/2)"
-          :key="n">
-        </v-row>
-      </v-col>
-      <v-col
-        class="red-outline">
-        <v-row
-          class="red-outline"
-          v-for="n in (Math.ceil((repositories?.length || 0)/2))"
-          :key="2*n+1">
-        </v-row>
-      </v-col>
+      <template
+        v-for="repo in repositories"
+        :key="repo.name">
+        <!-- https://vuetifyjs.com/en/components/cards/#combined -->
+        <v-col
+          md="4"
+          sm="6"
+          xs="12">
+          <v-card
+            class="mx-auto"
+            prepend-icon="mdi-github"
+            width="400">
+            <template v-slot:title>
+              <span class="font-weight-black">{{repo.name}}</span>
+            </template>
+            <v-card-subtitle>
+              {{repo.created_at}} <v-spacer></v-spacer> {{repo.license}}
+            </v-card-subtitle>
+            <v-card-text class="bg-surface-light pt-4">
+              {{repo.description}}
+            </v-card-text>
+            <v-card-actions>
+              <h5>Languages: {{repo.languages}}</h5>
+              <v-spacer></v-spacer>
+              <v-btn
+                text="Link"
+                prepend-icon="mdi-link-variant"
+                @click="openLink(repo.url)">
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </template>
     </v-row>
   </v-container>
-
 </template>
 <script setup>
 import axios from "axios";
 import {ref, reactive, onMounted } from 'vue';
 import { useI18n } from "vue-i18n";
-
+import {GITHUB_TOKEN} from "../../.env/dev.js";
 //components
-const { tm } = useI18n();
+const { t } = useI18n();
 const dataLoading = ref(true);
-//TODO
-const repositories = await getRepositories();
-console.log('repositories',repositories);
+const repositories = reactive([]);
+
+const BASE_URL = 'https://api.github.com';
 //functions
+const githubAPI = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+    Accept: 'application/vnd.github+json',
+  },
+});
+
+// Fetch repositories
 async function getRepositories() {
   let data;
-  axios.get("https://api.github.com/users/kszygenda/repos")
-    .then((response) => {
-      data = response.data.map((obj) => ({
+  try {
+    const response = await githubAPI.get('/users/kszygenda/repos');
+    data = await Promise.all(
+      response.data.map(async (obj) => ({
         name: obj.name,
-        url: obj.url,
-        created_at : obj.created_at,
-        languages: getProjectLanguages(obj.languages),
-        license: obj.license | tm("Repository_default_license_text"),
-        description: obj.description
-      }));
-    })
-    .catch((err) => {
-      alert(tm("Repositories_fetching_error") + err);
-      return [];
-    })
-    .finally(() => {
-      dataLoading.value = false;
-    })
+        url: obj.svn_url,
+        created_at: dateFormating(obj.created_at),
+        languages: languagesFormating(await getProjectLanguages(obj.languages_url)),
+        license: obj.license || t('RepositoriesView.Repository_default_license_text'),
+        description: obj.description,
+      }))
+    );
+  } catch (err) {
+    alert(t('RepositoriesView.Repositories_fetching_error') + err);
+    return [];
+  } finally {
+    dataLoading.value = false;
+  }
   return data;
 }
-async function getProjectLanguages(url) {
-  axios.get(url)
-    .then((response) => {
-      return Object.keys(response.data);
-    })
-    .catch((err) => {
-      alert(tm("Repository_languages_error") + err)
-      return [];
-    })
 
+// Fetch project languages
+async function getProjectLanguages(url) {
+  try {
+    const response = await githubAPI.get(url);
+    return response.data;
+  } catch (err) {
+    alert(t('RepositoriesView.Repository_languages_error') + err);
+    return [];
+  }
 }
+function dateFormating(date) {
+  return date.split("T")[0];
+}
+function languagesFormating(git_langs) {
+  return Object.keys(git_langs).toString();
+}
+function openLink(url) {
+  window.open(url,'_blank');
+}
+
+
 onMounted(() => {
-  getRepositories();
+  getRepositories().then((data) => {
+    for(let i = 0;i<data.length;++i){
+      repositories.push(data[i]);
+    }
+    console.log(repositories);
+  });
 })
 </script>
 
